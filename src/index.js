2ae0808ee3518082e6f36728f7460a22c3522fd6
jvasquezt2004 <jorgealonsovasquez@hotmail.com>
2024-04-20 15:22:16 -0600

    Agregados las dos rutas y los metodos junto con su estatus

--- src/index.js
+++ src/index.js
@@ -1,2 +1,25 @@
 const express = require("express");
 const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).send("Bienvenido a la pagina principal");
+});
+
+app.post("/", (req, res) => {
+  res.status(200) / json({ numero: Math.random() * 100 });
+});
+
+app.get("/login", (req, res) => {
+  res.status(200).send("Pagina de login");
+});
+
+app.post("/login", (req, res) => {
+  res
+    .status(200)
+    .json({ usuario: "user", sessionId: Math.floor(Math.random() * 100) });
+});
+
+const PORT = 8000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
