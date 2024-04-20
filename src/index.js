108aea9c7a943f770713212401bb0064172a1b07
jvasquezt2004 <jorgealonsovasquez@hotmail.com>
2024-04-20 15:35:14 -0600

    Agregada la libreria de faker

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 const express = require("express");
 const app = express();
+const faker = require("faker");
 
+app.use(express.json());
+
+const randomUserData = {
+  nombre: faker.name.findName(),
+  email: faker.internet.email(),
+  sitioWeb: faker.internet.url(),
+  direccion: {
+    ciudad: faker.address.city(),
+    calle: faker.address.streetName(),
+    pais: faker.address.country(),
+  },
+  imagenPerfil: faker.image.avatar(),
+  textoAleatorio: faker.lorem.paragraph(),
+};
+
+res.status(200).json(randomUserData);
 app.get("/", (req, res) => {
   res.status(200).send("Bienvenido a la pagina principal");
 });
 
 app.post("/", (req, res) => {
-  res.status(200) / json({ numero: Math.random() * 100 });
+  res.status(200).json(randomUserData);
 });
 
 app.get("/login", (req, res) => {
